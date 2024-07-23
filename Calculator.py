34e47cdd5cd2ba382c394d88be80d2a268d150dd
LibTower <119515731+LibTower@users.noreply.github.com>
2024-07-23 21:01:05 +0700

    add print and input
    add print and input for test

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+print('Введите числитель первой дроби: ')
+numerator1 = input()
+print('Введите знаменатель первой дроби: ')
+denominator1 = input()
+print('Введите числитель второй дроби: ')
+numerator2 = input()
+print('Введите знаменатель второй дроби: ')
+denominator2 = input()
+
+print('Ваши дроби: ')
+print('\n', numerator1 + '     ' + numerator2, '\n', '-' + '     ' + '-', '\n', denominator1+ '     ' + denominator2)
