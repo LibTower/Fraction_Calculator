a2b4fc8748f630219aba11bb2cff13a09d92d704
LibTower <khabarova.o.a@gmail.com>
2024-07-25 00:59:12 +0700

    delite coments

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-def LCM(a, b): ## Функция ищет наименьший общий делитель двух чисел поданых на вход
+def LCM(a, b):
     m = a * b
     while a != 0 and b != 0:
         if a > b:
@@ -23,20 +23,17 @@ print('Выбирите опирацию: ')
 print('1 - операция сложения\n2 - операция вычитания\n')
 choise = int(input())
 
-if(choise == 1): ## операция сложения
-    if(denominator1 == denominator2): ## операция сложения при одинаковом знаминателе 
-        resultNum = numerator1 + numerator2  ##складываем числители
-        print('Результат сложения:') ## выводим на экран все вырожение
+if(choise == 1): 
+    if(denominator1 == denominator2):
+        resultNum = numerator1 + numerator2  
+        print('Результат сложения:') 
         print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
-    else: ## Операция сложения при разных знаминателях
-        resultDem = LCM(denominator1, denominator2) ## с помощью функции поиска НОК находим общий знаминатель двух дробей
-        ##Находим числитель результата. Для этого числители двух преведущих дробей 
-        ##домножаем на то число, на которое необходимо домножить их знаминатели, для того что бы они стали равны
-        ##знаминателю результата.
+    else: 
+        resultDem = LCM(denominator1, denominator2) 
         resultNum = numerator1*(resultDem//denominator1) + numerator2*(resultDem//denominator2) 
-        print('Результат сложения:') ## выводим на экран все вырожение
+        print('Результат сложения:')
         print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
-elif(choise == 2): ## операция вычитания. Аналогична сложению но числители вычитаются из друг друга.
+elif(choise == 2): 
     if(denominator1 == denominator2):
         resultNum = numerator1 - numerator2
         print('Результат вычитания:')
