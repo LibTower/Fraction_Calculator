7d167c0d50d532e558c7da602fccd8ae217981b2
LibTower <khabarova.o.a@gmail.com>
2024-07-24 19:22:43 +0700

    subtraction with different denominators

--- Calculator.py
+++ Calculator.py
@@ -38,3 +38,9 @@ elif(choise == 2):
         resultNum = numerator1 - numerator2
         print('Результат вычитания:')
         print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
+    else:
+        resultDem = LCM(denominator1, denominator2)
+        resultNum = numerator1*(resultDem//denominator1) - numerator2*(resultDem//denominator2)
+        print('Результат вычитания:')
+        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
+ 
