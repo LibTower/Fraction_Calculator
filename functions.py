bd5ca17641fafc7c6a70527e9abe257a1762861d
LibTower <khabarova.o.a@gmail.com>
2024-07-25 01:30:04 +0700

    moved addition and subtraction to function.py as functions

--- functions.py
+++ functions.py
@@ -7,3 +7,19 @@ def LCM(a, b):
             b %= a
     return(m // (a + b))
 
+def sum(numerator1, denominator1, numerator2, denominator2):
+    if(denominator1 == denominator2):
+        resultNum = numerator1 + numerator2
+        resultDen = denominator1  
+    else:
+        resultDen = LCM(denominator1, denominator2) 
+        resultNum = numerator1*(resultDen//denominator1) + numerator2*(resultDen//denominator2) 
+    return resultNum, resultDen
+
+def difference(numerator1, denominator1, numerator2, denominator2):
+    if(denominator1 == denominator2):
+        resultNum = numerator1 - numerator2
+        resultDen = denominator1  
+    else:
+        resultDen = LCM(denominator1, denominator2) 
+        resultNum = numerator1*(resultDen//denominator1) - numerator2*(resultDen//denominator2) 
--- main.py
+++ main.py
@@ -14,41 +14,10 @@ print('Выбирите опирацию: ')
 print('1 - операция сложения\n2 - операция вычитания\n')
 choise = int(input())
 
-if(choise == 1): 
-    print('Введите числитель первой дроби: ')
-numerator1 = int(input())
-print('Введите знаменатель первой дроби: ')
-denominator1 = int(input())
-print('Введите числитель второй дроби: ')
-numerator2 = int(input())
-print('Введите знаменатель второй дроби: ')
-denominator2 = int(input())
+##if(choise == 1): 
 
-print('Ваши дроби: ')
-print('\n', numerator1, '     ', numerator2, '\n', '-', '     ', '-', '\n', denominator1, '     ', denominator2)
+##if(choise == 2):
 
-print('Выбирите опирацию: ')
-print('1 - операция сложения\n2 - операция вычитания\n')
-choise = int(input())
-
-if(choise == 1): 
-    if(denominator1 == denominator2):
-        resultNum = numerator1 + numerator2  
-        print('Результат сложения:') 
-        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
-    else: 
-        resultDem = LCM(denominator1, denominator2) 
-        resultNum = numerator1*(resultDem//denominator1) + numerator2*(resultDem//denominator2) 
-        print('Результат сложения:')
-        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
-elif(choise == 2): 
-    if(denominator1 == denominator2):
-        resultNum = numerator1 - numerator2
-        print('Результат вычитания:')
-        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
-    else:
-        resultDem = LCM(denominator1, denominator2)
-        resultNum = numerator1*(resultDem//denominator1) - numerator2*(resultDem//denominator2)
-        print('Результат вычитания:')
-        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
+##print('Результат вычитания:')
+##print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
  
