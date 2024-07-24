a7cbd44ee3a6e9152fb7a0545cd2f8ec6830808b
LibTower <khabarova.o.a@gmail.com>
2024-07-25 01:59:32 +0700

    fix input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import functions
+
 print('Введите числитель первой дроби: ')
 numerator1 = int(input())
 print('Введите знаменатель первой дроби: ')
@@ -10,14 +12,19 @@ denominator2 = int(input())
 print('Ваши дроби: ')
 print('\n', numerator1, '     ', numerator2, '\n', '-', '     ', '-', '\n', denominator1, '     ', denominator2)
 
-print('Выбирите опирацию: ')
-print('1 - операция сложения\n2 - операция вычитания\n')
-choise = int(input())
+print('Программа потдерживает 4 вида операций над дробями.')
+print('Сложение [+] \nРазность [-] \nУмножение [*] \nДеление [/] \n')
+print('Выберите одну из операций: ')
+choise = input()
 
-##if(choise == 1): 
-
-##if(choise == 2):
-
-##print('Результат вычитания:')
-##print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
+if(choise == '+'): 
+    Num, Den = functions.sum(numerator1, denominator1, numerator2, denominator2)
+elif(choise == '-'):
+    Num, Den = functions.difference(numerator1, denominator1, numerator2, denominator2)   
+elif(choise == '*'):
+    Num, Den = functions.multiplication(numerator1, denominator1, numerator2, denominator2)  
+elif(choise == '/'): 
+    Num, Den = functions.dividing(numerator1, denominator1, numerator2, denominator2)   
+print('Результат ')
+print('\n', numerator1,  '     ',  numerator2, '     ',  Num, '\n', '-',  ' ', choise, '  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', Den)
  
