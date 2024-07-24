9b4e219217b0c86dcb8e017ffdcec73612d79250
LibTower <khabarova.o.a@gmail.com>
2024-07-24 18:24:51 +0700

    addition with the same denominator

--- Calculator.py
+++ Calculator.py
@@ -1,23 +1,21 @@
 print('Введите числитель первой дроби: ')
-numerator1 = input()
+numerator1 = int(input())
 print('Введите знаменатель первой дроби: ')
-denominator1 = input()
+denominator1 = int(input())
 print('Введите числитель второй дроби: ')
-numerator2 = input()
+numerator2 = int(input())
 print('Введите знаменатель второй дроби: ')
-denominator2 = input()
+denominator2 = int(input())
 
 print('Ваши дроби: ')
-print('\n', numerator1 + '     ' + numerator2, '\n', '-' + '     ' + '-', '\n', denominator1+ '     ' + denominator2)
+print('\n', numerator1, '     ', numerator2, '\n', '-', '     ', '-', '\n', denominator1, '     ', denominator2)
 
 print('Выбирите опирацию: ')
 print('1 - опирация сложения\n2 - опирация вычитания\n')
-choise = input()
+choise = int(input())
 
 if(choise == 1):
-    print('*')
     if(denominator1 == denominator2):
-        print('*')
-        resultNumerator = numerator1 + numerator2
-        print('Результат сложения:\n')
-        print('\n', numerator1 + '     ' + numerator2 + '     ' + resultNumerator, '\n', '-' + '     ' + '-' + '     ' + '-', '\n', denominator1 + '     ' + denominator2 + '     ' + denominator2)
+        resultNum = numerator1 + numerator2
+        print('Результат сложения:')
+        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
