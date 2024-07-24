97d29d555bac44f9ab3bad941ebbc396f9614c8c
LibTower <khabarova.o.a@gmail.com>
2024-07-24 18:15:50 +0700

    variable type fix

--- Calculator.py
+++ Calculator.py
@@ -7,9 +7,17 @@ numerator2 = input()
 print('Введите знаменатель второй дроби: ')
 denominator2 = input()
 
-print('Выбирите опирацию: ')
-print('1 - опирация сложения\n 2 - опирация вычитания\n')
-choise = input()
-
 print('Ваши дроби: ')
 print('\n', numerator1 + '     ' + numerator2, '\n', '-' + '     ' + '-', '\n', denominator1+ '     ' + denominator2)
+
+print('Выбирите опирацию: ')
+print('1 - опирация сложения\n2 - опирация вычитания\n')
+choise = input()
+
+if(choise == 1):
+    print('*')
+    if(denominator1 == denominator2):
+        print('*')
+        resultNumerator = numerator1 + numerator2
+        print('Результат сложения:\n')
+        print('\n', numerator1 + '     ' + numerator2 + '     ' + resultNumerator, '\n', '-' + '     ' + '-' + '     ' + '-', '\n', denominator1 + '     ' + denominator2 + '     ' + denominator2)
