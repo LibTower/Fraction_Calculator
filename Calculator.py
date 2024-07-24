80962b8b5f046d8c5a16d13afce8a08e69f1ae36
LibTower <khabarova.o.a@gmail.com>
2024-07-24 19:21:43 +0700

    subtraction with one denominator

--- Calculator.py
+++ Calculator.py
@@ -33,3 +33,8 @@ if(choise == 1):
         resultNum = numerator1*(resultDem//denominator1) + numerator2*(resultDem//denominator2)
         print('Результат сложения:')
         print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', resultDem)
+elif(choise == 2):
+    if(denominator1 == denominator2):
+        resultNum = numerator1 - numerator2
+        print('Результат вычитания:')
+        print('\n', numerator1,  '     ',  numerator2, '     ',  resultNum, '\n', '-',  '  +  ',  '-',  '  =  ',  '-', '\n', denominator1, '     ', denominator2, '     ', denominator2)
