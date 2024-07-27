718b9619a3217298fb799cb58eb9b18ed0288ed1
LibTower <khabarova.o.a@gmail.com>
2024-07-27 21:51:10 +0700

    fix found errors

--- functions.py
+++ functions.py
@@ -1,27 +1,47 @@
+import sys
+
 def LCM(a, b): ## Поиск наименьшего общего кратного двух чисел a и b
-    m = a * b
-    while a != 0 and b != 0:
-        if a > b:
-            a %= b
-        else:
-            b %= a
-    return(m // (a + b))
+    if type(a) not in [int] or type(b) not in [int]:
+        raise ValueError('Было введено не число') 
+    if (a == 0 and b == 0):
+        raise ZeroDivisionError('Нет общего кратного больше 0')
+    else:
+        m = a * b
+        while a != 0 and b != 0:
+            if a > b:
+                a %= b
+            else:
+                b %= a
+        return(m // (a + b))
+
 
 def GCD(a, b): ## Поиск наибольшего общего делителя двух чисел a и b
-    while a != 0 and b != 0:
-        if a > b:
-            a %= b
-        else:
-            b %= a
-    return (a + b)
+    if type(a) not in [int] or type(b) not in [int]:
+        raise ValueError('Было введено не число') 
+    if (a == 0 and b == 0):
+        raise ZeroDivisionError('Нет общего делителя больше 0')
+    else:
+        while a != 0 and b != 0:
+            if a > b:
+                a %= b
+            else:
+                b %= a
+        return (a + b)
 
 def reducing_a_fraction(Numerator, Denominator): ## Сокращение дроби через НОД
+    if type(Numerator) not in [int] or type(Denominator) not in [int]:
+        raise ValueError('Было введено не число') 
     gcd = GCD(Numerator, Denominator)
-    Numerator//=gcd
-    Denominator//=gcd
-    return Numerator, Denominator
+    if(gcd == 0):
+        raise ZeroDivisionError('наибольший общий делитель равен 0')
+    else:
+        Numerator//=gcd
+        Denominator//=gcd
+        return Numerator, Denominator
 
 def sum(numerator1, denominator1, numerator2, denominator2): ##Операция сложения
+    if type(numerator1) not in [int] or type(numerator2) not in [int] or type(denominator1) not in [int] or type(denominator2) not in [int]:
+        raise ValueError('Было введено не число') 
     if(denominator1 == denominator2):
         resultNum = numerator1 + numerator2 ## Сложение дробей с одним знаминателем 
         resultDen = denominator1  
@@ -31,6 +51,8 @@ def sum(numerator1, denominator1, numerator2, denominator2): ##Операция 
     return resultNum, resultDen
 
 def difference(numerator1, denominator1, numerator2, denominator2): ##Операция вычитания
+    if type(numerator1) not in [int] or type(numerator2) not in [int] or type(denominator1) not in [int] or type(denominator2) not in [int]:
+        raise ValueError('Было введено не число') 
     if(denominator1 == denominator2):
         resultNum = numerator1 - numerator2 ## Сложение дробей с одним знаминателем
         resultDen = denominator1  
@@ -40,11 +62,15 @@ def difference(numerator1, denominator1, numerator2, denominator2): ##Опера
     return resultNum, resultDen
     
 def multiplication(numerator1, denominator1, numerator2, denominator2): ## Умножение дробей
+    if type(numerator1) not in [int] or type(numerator2) not in [int] or type(denominator1) not in [int] or type(denominator2) not in [int]:
+        raise ValueError('Было введено не число') 
     resultNum = numerator1 * numerator2
     resultDen = denominator1 * denominator2
     return resultNum, resultDen
 
 def dividing(numerator1, denominator1, numerator2, denominator2): ## Деление дробей
+    if type(numerator1) not in [int] or type(numerator2) not in [int] or type(denominator1) not in [int] or type(denominator2) not in [int]:
+        raise ValueError('Было введено не число') 
     resultNum = numerator1 * denominator2
     resultDen = denominator1 * numerator2
     return resultNum, resultDen
